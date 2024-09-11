aa3cbaa7c8b6e49c225918588b4c2544a41c2157
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-11 11:31:29 +0100

    Initial Commit

---
+++ Accessibility Sandbox/Accessibility_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_SandboxApp.swift
+//  Accessibility Sandbox
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
